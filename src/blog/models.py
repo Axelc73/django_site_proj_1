b1d7b15f04593018398114ee513ec4e8e80502b2
Axelc73 <acristolo@outlook.fr>
2024-11-20 17:13:54 +0100

    first commit

--- src/blog/models.py
+++ src/blog/models.py
@@ -4,5 +4,5 @@ class Creation(models.Model):
 
     nom = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=False, blank=False)
 # Create your models here.
